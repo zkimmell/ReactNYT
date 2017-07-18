e94d749afd6c91be0b0c109a22ec7746d36b1f7e
zkimmell <zkimmell@yahoo.com>
2017-07-17 21:37:46 -0500

    Update helpers.js

---
+++ app/utils/helpers.js
@@ -0,0 +1,67 @@
+//Axios library for HTTP requests
+var axios = require("axios");
+
+//API key
+var APIKey = "9b3adf57854f4a19b7b5782cdd6e427a";
+
+
+var helpers = {
+
+  runQuery: function(term, start, end) {
+
+    // Adjust to get search terms in proper format
+    var formattedTerm = term.trim();
+    var formattedStart = start.trim() + "0101";
+    var formattedEnd = end.trim() + "1231";
+
+
+    console.log("Query Run");
+    // Return the results from Axios query as an object with an array.
+    return axios.get("https://api.nytimes.com/svc/search/v2/articlesearch.json", {
+      params: {
+        "api-key": APIKey,
+        "q": formattedTerm,
+        "begin_date": formattedStart,
+        "end_date": formattedEnd
+      }
+    })
+    .then(function(results) {
+      console.log("Axios Results", results.data.response);
+      return results.data.response;
+    });
+  },
+  // Return any saved articles from db
+  getSaved: function() {
+    return axios.get("/api/saved")
+      .then(function(results) {
+        console.log("axios results", results);
+        return results;
+      });
+  },
+  // Save new articles to db
+  postSaved: function(title, date, url) {
+    var newArticle = { title: title, date: date, url: url };
+    return axios.post("/api/saved", newArticle)
+      .then(function(response) {
+        console.log("axios results", response.data._id);
+        return response.data._id;
+      });
+  },
+  // Remove saved articles from db
+  deleteSaved: function(title, data, url) {
+    return axios.delete("/api/saved", {
+      params: {
+        "title": title,
+        "data": data,
+        "url": url
+      }
+    })
+    .then(function(results) {
+      console.log("axios results", results);
+      return results;
+    });
+  }
+};
+
+
+module.exports = helpers;
